33d8162265e8b8d2b8d25f6aa3919f91d874458b
Farukh <excommunicad00@yandex.ru>
2024-05-27 19:02:57 +0600

    fix: unused Imports

--- stats/Statistics-Service/src/main/java/ru/practicum/mapper/StatisticMapper.java
+++ stats/Statistics-Service/src/main/java/ru/practicum/mapper/StatisticMapper.java
@@ -4,11 +4,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import ru.practicum.dto.EndpointDto;
-import ru.practicum.dto.ViewStatsDto;
 import ru.practicum.model.Statistic;
-import ru.practicum.model.StatisticProjection;
-
-import java.util.List;
 
 import static ru.practicum.constant.StatisticConstant.DATE_TIME_FORMATTER;
 
@@ -19,9 +15,4 @@ public interface StatisticMapper {
             @Mapping(target = "timestamp", source = "timestamp", dateFormat = DATE_TIME_FORMATTER)
     })
     Statistic toEndpointDto(EndpointDto endpointDto);
-
-//    List<ViewStatsDto> toViewDto(List<StatisticProjection> viewStatsDto);
-    @Mapping(target = "id", ignore = true)
-    List<ViewStatsDto> toViewDto(List<Statistic> viewStatsDto);
-
 }
